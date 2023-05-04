50ec685fed5077cbaf41eb24615f66c5e38d9b85
Mounir Nessim <mnessim@ad.wcpss.net>
2023-05-04 12:15:20 -0400

    Minor header changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './header.css'
 
 export function Header() {
     return (
+        <div>
         <div className="container-fluid row">
-            <h1 className="display-3 fw-bold">Mounir Nessim</h1>
+            <h1 className="display-3 fw-bold col-9">Mounir Nessim</h1><span className="login-btn col-3"><Link to={'/coding/login'} className="navlink">Login</Link> </span>
+        </div>
+        <div className="row container">
             <h2 className="display-6">Educator, Programmer, Musician</h2>
         </div>
+        </div>  
     )
 }
